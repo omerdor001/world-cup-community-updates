f535c2c27187625ac8198da71d0013dc22ff9700
OmerDor001 <102467386+omerdor001@users.noreply.github.com>
2024-10-06 15:55:03 +0300

    Add files via upload

---
+++ server/src/impl/echo/EchoServer.java
@@ -0,0 +1,21 @@
+package bgu.spl.net.impl.echo;
+
+public class EchoServer {
+
+    public static void main(String[] args) {
+
+        // you can use any server... 
+        //Server.threadPerClient(
+        //        7777, //port
+       //         () -> new EchoProtocol(), //protocol factory
+        //        LineMessageEncoderDecoder::new //message encoder decoder factory
+       // ).serve();
+
+        // Server.reactor(
+        //         Runtime.getRuntime().availableProcessors(),
+        //         7777, //port
+        //         () -> new EchoProtocol<>(), //protocol factory
+        //         LineMessageEncoderDecoder::new //message encoder decoder factory
+        // ).serve();
+    }
+}
